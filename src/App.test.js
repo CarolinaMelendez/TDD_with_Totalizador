5d6026b26978991fdfbd8ecb54b445efecef74a7
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-09-29 01:04:15 -0400

    Prueba 4 - PASS

--- src/App.test.js
+++ src/App.test.js
@@ -13,10 +13,15 @@ describe("calcular total", () => {
   it("Prueba 3 - get total price with tax", () => {
     expect(calcularTotal(20,10,10)).toEqual(220);
   });
+  it("Prueba 4 - total price with discount (1 condition)", () => {
+    expect(calcularTotal(20,100,10)).toEqual(2000);
+  });
 });
 
 function calcularTotal(price,quantity,tax){
-    total_price = price*quantity
-    calculated_tax = total_price * (tax/100)
-    return total_price + calculated_tax;
+    sum_total_price = price*quantity;
+    calculated_tax = sum_total_price * (tax/100);
+    total_witn_tax = sum_total_price + calculated_tax;
+    if (quantity >= 100 ) return total_witn_tax - (sum_total_price*0.10)
+    return total_witn_tax;
 }
