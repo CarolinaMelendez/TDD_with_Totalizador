793b801d7dd6571838fa713ee854591ab5eb50d4
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-09-29 00:57:44 -0400

    Prueba 3 - PASS

--- src/App.test.js
+++ src/App.test.js
@@ -2,16 +2,21 @@
 
 describe("calcular total", () => {
   it("Prueba 1.1 - get price", () => {
-    expect(calcularTotal(10,1)).toEqual(10);
+    expect(calcularTotal(10,1,0)).toEqual(10);
   });
   it("Prueba 1.2 - get price", () => {
-    expect(calcularTotal(20,1)).toEqual(20);
+    expect(calcularTotal(20,1,0)).toEqual(20);
   });
   it("Prueba 2 - get total price", () => {
-    expect(calcularTotal(20,10)).toEqual(200);
+    expect(calcularTotal(20,10,0)).toEqual(200);
+  });
+  it("Prueba 3 - get total price with tax", () => {
+    expect(calcularTotal(20,10,10)).toEqual(220);
   });
 });
 
-function calcularTotal(precio,quantity){
-    return precio*quantity;
+function calcularTotal(price,quantity,tax){
+    total_price = price*quantity
+    calculated_tax = total_price * (tax/100)
+    return total_price + calculated_tax;
 }
