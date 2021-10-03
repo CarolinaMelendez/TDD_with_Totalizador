1419fd77e1bbedde370cf82a3cfa5dbc8aa4e6db
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-10-02 23:12:01 -0400

    Here - start again with totalizer

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,7 @@
 // import calcularTotal from "./App.js";
+import {calculateTotal } from "./Functions";
 
-describe("calcular total", () => {
-  it("Prueba 1.1 - get price", () => {
-    expect(calcularTotal(10,1,0)).toEqual(10);
-  });
-  it("Prueba 1.2 - get price", () => {
-    expect(calcularTotal(20,1,0)).toEqual(20);
-  });
-  it("Prueba 2 - get total price", () => {
-    expect(calcularTotal(20,10,0)).toEqual(200);
-  });
-  it("Prueba 3 - get total price with tax", () => {
-    expect(calcularTotal(20,10,10)).toEqual(220);
-  });
-  it("Prueba 4 - total price with discount (1 condition)", () => {
-    expect(calcularTotal(20,1000,10)).toEqual(21400);
-  });
-  it("Prueba 4 - total price with discount (2 condition)", () => {
-    expect(calcularTotal(20,3000,10)).toEqual(63000);
-  });
+describe("-- Totalizer --", () => {
+
 });
 
-function calcularTotal(price,quantity,tax){
-    sum_total_price = price*quantity;
-    calculated_tax = sum_total_price * (tax/100);
-    total_with_tax = sum_total_price + calculated_tax;
-    if (has_it_discount(quantity, 3000)) return total_with_tax - (sum_total_price*0.05)
-    if (has_it_discount(quantity, 1000)) return total_with_tax - (sum_total_price*0.03)
-    return total_with_tax;
-}
-
-function has_it_discount(quantity, minimum_quantity){
-    return quantity >= minimum_quantity
-}
--- src/Functions.js
+++ src/Functions.js
@@ -5,27 +5,37 @@ module.exports = {
 }
 */
 
-function control_bad_inputs(document,name_item,quantity ,price){
-    badInput = false;
-    if(!isNaN(name_item.value)){
-        document.write("<p> UUUUuuppss ¡¡¡  <br/>");
-        document.write(name_item.value + " no es un nombre de item valido :P  <br/>");
-        document.write(" Por favor, vuelva a hacer cargar la pagina e intentelo de nuevo <br/><br/> </p>");
-        badInput = true;
-    }
-    if (quantity.value <= 0){
-        document.write("<p> El valor de la cantidad no es valido!!! <br/><br/> </p>");
-        badInput = true;
-    }
-    if (price.value <= 0){
-        document.write("<p> El valor del precio no es valido!!! <br/><br/> </p>");
-        badInput = true;
-    }
-    if (badInput){
-        document.write("<img src=\"https://bit.ly/3zLJARJ\"");
-    }
+export function calculateTotal(price,quantity,tax){
+
 }
 
-export const My_Functions = {
-    control_bad_inputs
-};
+
+/*
+export function calcularTotal(price,quantity,tax){
+    let sum_total_price = price*quantity;
+    let calculated_tax = sum_total_price * (tax/100);
+    let total_with_tax = sum_total_price + calculated_tax;
+    if (has_it_discount(quantity, 3000)) return total_with_tax - (sum_total_price*0.05)
+    if (has_it_discount(quantity, 1000)) return total_with_tax - (sum_total_price*0.03)
+    return total_with_tax;
+}
+
+function has_it_discount(quantity, minimum_quantity){
+    return quantity >= minimum_quantity
+}
+*/
+
+/*
+
+function impuestoEstado(estado) {
+    let impuestos = { CA: 0.0825, UT: 0.0665, "": 0 };
+    return impuestos[estado];
+  }
+  
+  function calcularTotal(cantidad, precio, estado) {
+    let subTotal = cantidad * precio;
+    let impuesto = impuestoEstado(estado);
+    subTotal = subTotal + subTotal * impuesto;
+    return subTotal;
+  }
+  */
